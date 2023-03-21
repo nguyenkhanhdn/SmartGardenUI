28fa1b77020c5e14c9e47dd8a4a8339cfb692a7e
Khanh Nguyen <khanhdsp@gmail.com>
2023-03-21 23:11:35 +0700

    add new features

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -29,20 +29,23 @@ namespace SmartGardenUI
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.ribbon1 = new System.Windows.Forms.Ribbon();
             this.ribbonTab1 = new System.Windows.Forms.RibbonTab();
             this.ribbonPanel1 = new System.Windows.Forms.RibbonPanel();
-            this.ribbonTab2 = new System.Windows.Forms.RibbonTab();
-            this.ribbonPanel2 = new System.Windows.Forms.RibbonPanel();
-            this.ribbonPanel3 = new System.Windows.Forms.RibbonPanel();
-            this.ribbonPanel4 = new System.Windows.Forms.RibbonPanel();
             this.ribbonButton1 = new System.Windows.Forms.RibbonButton();
             this.ribbonButton2 = new System.Windows.Forms.RibbonButton();
+            this.ribbonPanel2 = new System.Windows.Forms.RibbonPanel();
+            this.ribbonTab2 = new System.Windows.Forms.RibbonTab();
+            this.ribbonPanel3 = new System.Windows.Forms.RibbonPanel();
+            this.ribbonPanel4 = new System.Windows.Forms.RibbonPanel();
             this.btnClose = new System.Windows.Forms.Button();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -70,6 +73,7 @@ namespace SmartGardenUI
             this.ribbon1.Tabs.Add(this.ribbonTab1);
             this.ribbon1.Tabs.Add(this.ribbonTab2);
             this.ribbon1.Text = "ribbon1";
+            this.ribbon1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ribbon1_MouseDown);
             // 
             // ribbonTab1
             // 
@@ -84,28 +88,6 @@ namespace SmartGardenUI
             this.ribbonPanel1.Items.Add(this.ribbonButton2);
             this.ribbonPanel1.Name = "ribbonPanel1";
             this.ribbonPanel1.Text = "System";
-            // 
-            // ribbonTab2
-            // 
-            this.ribbonTab2.Name = "ribbonTab2";
-            this.ribbonTab2.Panels.Add(this.ribbonPanel3);
-            this.ribbonTab2.Panels.Add(this.ribbonPanel4);
-            this.ribbonTab2.Text = "About";
-            // 
-            // ribbonPanel2
-            // 
-            this.ribbonPanel2.Name = "ribbonPanel2";
-            this.ribbonPanel2.Text = "Settings";
-            // 
-            // ribbonPanel3
-            // 
-            this.ribbonPanel3.Name = "ribbonPanel3";
-            this.ribbonPanel3.Text = "ribbonPanel3";
-            // 
-            // ribbonPanel4
-            // 
-            this.ribbonPanel4.Name = "ribbonPanel4";
-            this.ribbonPanel4.Text = "ribbonPanel4";
             // 
             // ribbonButton1
             // 
@@ -122,6 +104,28 @@ namespace SmartGardenUI
             this.ribbonButton2.Name = "ribbonButton2";
             this.ribbonButton2.SmallImage = ((System.Drawing.Image)(resources.GetObject("ribbonButton2.SmallImage")));
             this.ribbonButton2.Text = "Button 2";
+            // 
+            // ribbonPanel2
+            // 
+            this.ribbonPanel2.Name = "ribbonPanel2";
+            this.ribbonPanel2.Text = "Settings";
+            // 
+            // ribbonTab2
+            // 
+            this.ribbonTab2.Name = "ribbonTab2";
+            this.ribbonTab2.Panels.Add(this.ribbonPanel3);
+            this.ribbonTab2.Panels.Add(this.ribbonPanel4);
+            this.ribbonTab2.Text = "About";
+            // 
+            // ribbonPanel3
+            // 
+            this.ribbonPanel3.Name = "ribbonPanel3";
+            this.ribbonPanel3.Text = "ribbonPanel3";
+            // 
+            // ribbonPanel4
+            // 
+            this.ribbonPanel4.Name = "ribbonPanel4";
+            this.ribbonPanel4.Text = "ribbonPanel4";
             // 
             // btnClose
             // 
@@ -148,6 +152,7 @@ namespace SmartGardenUI
             // 
             // splitContainer1.Panel1
             // 
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
             this.splitContainer1.Panel1.Controls.Add(this.button1);
             this.splitContainer1.Panel1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             // 
@@ -160,6 +165,15 @@ namespace SmartGardenUI
             this.splitContainer1.SplitterDistance = 293;
             this.splitContainer1.TabIndex = 2;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // pictureBox1
             // 
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -170,14 +184,18 @@ namespace SmartGardenUI
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // button1
+            // timer1
             // 
-            this.button1.Location = new System.Drawing.Point(44, 93);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(115, 79);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(41, 99);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(209, 26);
+            this.textBox1.TabIndex = 1;
             // 
             // frmMain
             // 
@@ -192,7 +210,10 @@ namespace SmartGardenUI
             this.Name = "frmMain";
             this.Text = "Smart Garden Detector";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.Load += new System.EventHandler(this.frmMain_Load);
             this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
@@ -216,6 +237,8 @@ namespace SmartGardenUI
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- frmMain.cs
+++ frmMain.cs
@@ -1,9 +1,17 @@
-﻿using System;
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using lobe;
+using lobe.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Media;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +20,44 @@ namespace SmartGardenUI
 {
     public partial class frmMain : Form
     {
+        private VideoCaptureDevice captureDevice;
+        private FilterInfoCollection filterInfo;
+
+        private bool cameraOn = false;
+
+        private SoundPlayer simpleSound = new SoundPlayer(Environment.CurrentDirectory + @"\sound\alarm.wav");
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        void StartCamera()
+        {
+            try
+            {
+                filterInfo = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                captureDevice = new VideoCaptureDevice(filterInfo[0].MonikerString);
+
+                captureDevice.NewFrame += new NewFrameEventHandler(CameraOn);
+                if (cameraOn)
+                {
+                    captureDevice.Start();
+                }
+                else
+                {
+                    captureDevice.Stop();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
         public frmMain()
         {
             InitializeComponent();
@@ -23,6 +69,126 @@ namespace SmartGardenUI
             {
                 Application.Exit();
             }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //string imageDir = Path.Combine(Environment.CurrentDirectory, "..", "..");
+            var bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.DrawToBitmap(bitmap, pictureBox1.ClientRectangle);
+           
+            byte[] bytes = ImageToByte2(bitmap);
+            string className = Predict(bytes);
+            ProcessResult(className);
+        }
+        private void ProcessResult(string level)
+        {
+            this.textBox1.Text = level;
+
+            //if (level == "nofire")
+            //{
+            //    timer1.Enabled = false;
+            //    textBox2.Text = "Bình thường";
+            //    textBox3.Text = "TẮT";
+            //    SendToCloud(0, "0", "0");
+            //    simpleSound.Stop();
+            //    MessageBox.Show("0");
+            //    timer1.Enabled = true;
+
+            //}
+            
+        }
+
+        private string Predict(string fileName)
+        {
+            var signatureFilePath = Environment.CurrentDirectory + @"\model\signature.json";
+            var imageToClassify = fileName;
+
+            lobe.ImageClassifier.Register("onnx", () => new OnnxImageClassifier());
+            var classifier = ImageClassifier.CreateFromSignatureFile(new FileInfo(signatureFilePath));
+
+            //var classifier2 = ImageClassifier.CreateFromSignatureFile(new FileInfo(signatureFilePath));
+
+            var results = classifier.Classify(SixLabors.ImageSharp.Image.Load(imageToClassify).CloneAs<Rgb24>());
+            return results.Prediction.Label;
+        }
+
+        private string Predict(byte[] bitmap)
+        {
+            var signatureFilePath = Environment.CurrentDirectory + @"\model\signature.json";
+
+            lobe.ImageClassifier.Register("onnx", () => new OnnxImageClassifier());
+            var classifier = ImageClassifier.CreateFromSignatureFile(new FileInfo(signatureFilePath));
+
+            var results = classifier.Classify(SixLabors.ImageSharp.Image.Load(bitmap).CloneAs<Rgb24>());
+            return results.Prediction.Label;
+        }
+
+        public static byte[] ImageToByte2(Image img)
+        {
+            using (var stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
+        }
+        private void SendToCloud(int level, string desc, string onOff)
+        {
+            try
+            {
+                const string WRITEKEY = "TLCC0QSDXNRF07ZD";
+                string strUpdateBase = "http://api.thingspeak.com/update";
+                string strUpdateURI = strUpdateBase + "?key=" + WRITEKEY;
+                string strField1 = level.ToString();
+                string strField2 = desc;
+                string strField3 = onOff;
+                HttpWebRequest ThingsSpeakReq;
+                HttpWebResponse ThingsSpeakResp;
+
+                strUpdateURI += "&field1=" + strField1;
+                strUpdateURI += "&field2=" + strField2;
+                strUpdateURI += "&field3=" + strField3;
+
+                ThingsSpeakReq = (HttpWebRequest)WebRequest.Create(strUpdateURI);
+
+                ThingsSpeakResp = (HttpWebResponse)ThingsSpeakReq.GetResponse();
+
+                if (!(string.Equals(ThingsSpeakResp.StatusDescription, "OK")))
+                {
+                    Exception exData = new Exception(ThingsSpeakResp.StatusDescription);
+                    //throw exData;
+                }
+            }
+            catch (Exception ex)
+            {
+                this.Text = ex.Message;
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ribbon1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+        private void CameraOn(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBox1.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            captureDevice.Stop();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            cameraOn = true;
+            StartCamera();
         }
     }
 }
